87b17f93bcf4b1faa7bbddf96acd846dfed670d8
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-15 12:43:08 +0300

    Изучение паттернов

--- src/main/java/cor/youtube/ChainOfResponsibility.java
+++ src/main/java/cor/youtube/ChainOfResponsibility.java
@@ -2,6 +2,52 @@ package main.java.cor.youtube;
 
 public class ChainOfResponsibility {
     public static void main(String[] args) {
+        MessagePrinter messagePrinter = new ConsoleMessagePrinter();
+        FileMessagePrinter fileMessagePrinter = new FileMessagePrinter();
+        messagePrinter.setNextMessagePrinter(fileMessagePrinter);
+        fileMessagePrinter.setNextMessagePrinter(new DBMessagePrinter());
+        messagePrinter.print("Hello");
+    }
+}
 
+
+abstract class MessagePrinter {
+    MessagePrinter nextMessagePrinter;
+
+    void setNextMessagePrinter(MessagePrinter messagePrinter) {
+        this.nextMessagePrinter = messagePrinter;
+    }
+
+    void print(String message) {
+        printMessage(message);
+        if (nextMessagePrinter != null) {
+            nextMessagePrinter.print(message);
+        }
+    }
+
+    abstract void printMessage(String message);
+}
+
+class ConsoleMessagePrinter extends MessagePrinter {
+
+    @Override
+    void printMessage(String message) {
+        System.out.println("print to console: " + message);
+    }
+}
+
+class FileMessagePrinter extends MessagePrinter {
+
+    @Override
+    void printMessage(String message) {
+        System.out.println("print to file: " + message);
+    }
+}
+
+class DBMessagePrinter extends MessagePrinter {
+
+    @Override
+    void printMessage(String message) {
+        System.out.println("print to data base: " + message);
     }
 }
